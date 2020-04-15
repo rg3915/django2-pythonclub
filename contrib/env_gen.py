17f111ebeb74d7af6c5411a1b745eb587f7aeee5
Juliano Atanazio <juliano777@gmail.com>
2020-04-15 10:44:43 -0300

    Format string :)

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -8,11 +8,11 @@ from django.utils.crypto import get_random_string
 
 chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 
-CONFIG_STRING = """
+CONFIG_STRING = f"""
 DEBUG=True
-SECRET_KEY=%s
-ALLOWED_HOSTS=127.0.0.1, .localhost
-#DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
+SECRET_KEY='{get_random_string(50, chars)}'
+ALLOWED_HOSTS='127.0.0.1, localhost'
+#DATABASE_URL='postgres://USER:PASSWORD@HOST:PORT/NAME'
 #DEFAULT_FROM_EMAIL=
 #EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
 #EMAIL_HOST=
@@ -20,7 +20,7 @@ ALLOWED_HOSTS=127.0.0.1, .localhost
 #EMAIL_USE_TLS=
 #EMAIL_HOST_USER=
 #EMAIL_HOST_PASSWORD=
-""".strip() % get_random_string(50, chars)
+""".strip()
 
 # Writing our configuration file to '.env'
 with open('.env', 'w') as configfile:
