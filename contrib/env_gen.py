cd4c56c5b0ea88c1cbebaf30d71857a43b4699e9
Juliano Atanazio <juliano777@gmail.com>
2020-04-15 11:08:23 -0300

    Espaço para o sinal de igual

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -10,16 +10,16 @@ chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 
 CONFIG_STRING = f"""
 DEBUG=True
-SECRET_KEY='{get_random_string(50, chars)}'
-ALLOWED_HOSTS='127.0.0.1, localhost'
-#DATABASE_URL='postgres://USER:PASSWORD@HOST:PORT/NAME'
-#DEFAULT_FROM_EMAIL=
-#EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
-#EMAIL_HOST=
-#EMAIL_PORT=
-#EMAIL_USE_TLS=
-#EMAIL_HOST_USER=
-#EMAIL_HOST_PASSWORD=
+SECRET_KEY = '{get_random_string(50, chars)}'
+ALLOWED_HOSTS = '127.0.0.1, localhost'
+#DATABASE_URL = 'postgres://USER:PASSWORD@HOST:PORT/NAME'
+#DEFAULT_FROM_EMAIL =
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST =
+#EMAIL_PORT =
+#EMAIL_USE_TLS =
+#EMAIL_HOST_USER =
+#EMAIL_HOST_PASSWORD =
 """.strip()
 
 # Writing our configuration file to '.env'
